aabf55535f10172ac49d8516ecb441f755c65871
Tusk <Tuskman045@gmail.com>
2025-02-02 19:15:25 -0700

    Add project files.

---
+++ evilcalculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("evilcalculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University")>
+<Assembly: AssemblyProduct("evilcalculator")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("314dc590-d4f3-41a9-b4e0-e079db8d2d5b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ evilcalculator/evilcalculator.vb
@@ -0,0 +1,79 @@
+﻿Module evilcalculator
+
+    Sub Main()
+
+        Dim userInput As String
+        Dim getoutofhere As Boolean = False
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        Do
+            Do
+                Console.WriteLine("Please enter two numbers.")
+                Console.WriteLine("Enter Q to quit")
+                userInput = Console.ReadLine
+                Console.WriteLine($"You entered {userInput}")
+
+                Try
+                    firstNumber = CInt(userInput)
+                    getoutofhere = True
+
+                Catch ex As Exception
+
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not a whole number.")
+                    Else
+                        getoutofhere = True
+
+                    End If
+                End Try
+            Loop Until getoutofhere = True OrElse userInput <> "Q"
+
+            getoutofhere = False
+            Do While userInput <> "Q" And getoutofhere = False
+                Console.WriteLine("Enter a second number")
+                Console.WriteLine("Enter Q to quit")
+                userInput = Console.ReadLine
+                Console.WriteLine($"You entered {userInput}")
+
+                Try
+                    secondNumber = CInt(userInput)
+                    getoutofhere = True
+
+                Catch ex As Exception
+
+                    If userInput <> "Q" Then
+                        Console.WriteLine($"{userInput} is not a whole number.")
+                    Else
+                        getoutofhere = True
+
+                    End If
+                End Try
+            Loop
+
+            Console.WriteLine("Select One." & vbNewLine _
+                         & "1. Add" & vbNewLine _
+                         & "2. Subtract" & vbNewLine _
+                         & "3. Multiply" & vbNewLine _
+                         & "4. Divide")
+            userInput = Console.ReadLine
+            If userInput = "1" Then
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+            ElseIf userInput = "2" Then
+                Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
+            ElseIf userInput = "3" Then
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
+            ElseIf userInput = "4" Then
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
+            Else
+                Console.WriteLine($"You entered {userInput}, please enter a whole number.")
+            End If
+
+
+        Loop While userInput <> "Q"
+        Console.WriteLine("Have a nice day")
+        Console.WriteLine("Press enter to close this window")
+
+    End Sub
+
+End Module
