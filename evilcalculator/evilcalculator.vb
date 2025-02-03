f5c41a4a03cd0a6bd8fb2d6b8dfe42fa298fc50a
Tusk <Tuskman045@gmail.com>
2025-02-02 19:50:14 -0700

    Add addition, subtraction, multiplication, division

--- evilcalculator/evilcalculator.vb
+++ evilcalculator/evilcalculator.vb
@@ -1,4 +1,12 @@
-﻿Module evilcalculator
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'Better Calculator
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module evilcalculator
 
     Sub Main()
 
@@ -8,9 +16,8 @@
         Dim secondNumber As Integer
 
         Do
-            Do
-                Console.WriteLine("Please enter two numbers.")
-                Console.WriteLine("Enter Q to quit")
+            Do Until userInput = "Q" Or getoutofhere = True
+                Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.")
                 userInput = Console.ReadLine
                 Console.WriteLine($"You entered {userInput}")
 
@@ -27,17 +34,15 @@
 
                     End If
                 End Try
-            Loop Until getoutofhere = True OrElse userInput <> "Q"
+            Loop
 
             getoutofhere = False
-            Do While userInput <> "Q" And getoutofhere = False
-                Console.WriteLine("Enter a second number")
-                Console.WriteLine("Enter Q to quit")
+            Do Until userInput = "Q" Or getoutofhere = True
                 userInput = Console.ReadLine
                 Console.WriteLine($"You entered {userInput}")
 
                 Try
-                    secondNumber = CInt(userInput)
+                    firstNumber = CInt(userInput)
                     getoutofhere = True
 
                 Catch ex As Exception
@@ -51,27 +56,35 @@
                 End Try
             Loop
 
-            Console.WriteLine("Select One." & vbNewLine _
+            getoutofhere = False
+            Do Until userInput = "Q" Or getoutofhere = True
+                Console.WriteLine("Select One." & vbNewLine _
                          & "1. Add" & vbNewLine _
                          & "2. Subtract" & vbNewLine _
                          & "3. Multiply" & vbNewLine _
                          & "4. Divide")
-            userInput = Console.ReadLine
-            If userInput = "1" Then
-                Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
-            ElseIf userInput = "2" Then
-                Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
-            ElseIf userInput = "3" Then
-                Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
-            ElseIf userInput = "4" Then
-                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
-            Else
-                Console.WriteLine($"You entered {userInput}, please enter a whole number.")
-            End If
+                userInput = Console.ReadLine
+                If userInput = "1" Then
+                    getoutofhere = True
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+                ElseIf userInput = "2" Then
+                    getoutofhere = True
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
+                ElseIf userInput = "3" Then
+                    getoutofhere = True
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
+                ElseIf userInput = "4" Then
+                    getoutofhere = True
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
+                Else
+                    Console.WriteLine($"You entered {userInput}, please enter a whole number.")
+                End If
+            Loop
+            getoutofhere = False
 
 
-        Loop While userInput <> "Q"
-        Console.WriteLine("Have a nice day")
+        Loop Until userInput = "Q"
+            Console.WriteLine("Have a nice day")
         Console.WriteLine("Press enter to close this window")
 
     End Sub
